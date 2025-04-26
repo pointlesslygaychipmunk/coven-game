81d359b06c3dba74b9cef38765678d5bd86b56cb
pointlesslygaychipmunk <mileslefay@gmail.com>
2025-04-26 11:01:08 -0700

    Delete backend/dist directory

--- backend/dist/shared/scoreLogic.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateScore = void 0;
-function calculateScore(state) {
-    // paste the implementation from backend/src/gameOverLogic.ts
-}
-exports.calculateScore = calculateScore;
