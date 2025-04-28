c3a98d29a98606e1bd0238599cfc5b32dd41ac39
pointlesslygaychipmunk <mileslefay@Miless-MacBook-Air.local>
2025-04-27 23:11:29 -0700

    the coven alpha 4.2.0

--- frontend/src/utils/reducer.ts
+++ frontend/src/utils/reducer.ts
@@ -3,7 +3,7 @@ import type { GameState, Action } from "@shared/types";
 export function reducer(state: GameState, action: Action): GameState {
   switch (action.type) {
     case "loadState":
-      return action.state;
+      return action.state;  // FULL STATE REPLACEMENT
     case "noop":
       return state;
     case "plant":
